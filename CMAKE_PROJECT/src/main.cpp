8b3588535b5c18e603c4ed8da218c5a546581505
crackdoctor@163.com <crackdoctor@163.com>
2021-05-08 10:35:14 +0800

    添加 导出类

--- CMAKE_PROJECT/src/main.cpp
+++ CMAKE_PROJECT/src/main.cpp
@@ -1,4 +1,23 @@
 #include <iostream>
+#include "CMAKE_PROJECTExport.h"
+
+class CMAKE_PROJECT_EXPORT MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+
+private:
+
+};
+
+MyClass::MyClass()
+{
+}
+
+MyClass::~MyClass()
+{
+} 
 int main()
 {
 	return 0;
